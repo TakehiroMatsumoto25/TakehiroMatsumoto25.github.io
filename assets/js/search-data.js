76e459f45c16d06b6f4b3559a6c1b50945f40d2d
TakehiroMatsumoto25 <takehiro.m.jp@gmail.com>
2026-02-26 05:12:43 +0000

    Deploying to gh-pages from @ TakehiroMatsumoto25/TakehiroMatsumoto25.github.io@c821a453819af44c13f15759a312deaec62afb49 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,6 +30,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-cv",
+          title: "CV",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
+          },
         },{id: "nav-myself",
           title: "myself",
           description: "",
